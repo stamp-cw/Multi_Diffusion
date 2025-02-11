9b23cb3a5169369ddd3f391e35c9ff2ebd43ee6c
75889 <admin@admin>
2025-02-11 15:28:20 +0800

    修复Xt

--- src/nb_diffusion.py
+++ src/nb_diffusion.py
@@ -185,11 +185,11 @@ class NBDiffusion:
         model_mean, _, model_log_variance = self.p_mean_variance(model, x_t, t,
                                                     clip_denoised=clip_denoised)
 
-        # noise_coef1 = self._extract(self.noise_coef1, t, x_t.shape)
+        noise_coef1 = self._extract(self.noise_coef1, t, x_t.shape)
         # noise = noise_coef1 * torch.randn_like(x_start)
 
-        # noise = noise_coef1 * torch.randn_like(x_t)
-        noise = torch.randn_like(x_t)#这里是采样用作的标准高斯噪声
+        noise = noise_coef1 * torch.randn_like(x_t)
+        # noise = torch.randn_like(x_t)#这里是采样用作的标准高斯噪声
 
         # pred_img = model_mean + torch.sqrt(model_variance).float() * noise
         pred_img = model_mean + nonzero_mask * (0.5 * model_log_variance).exp() * noise
