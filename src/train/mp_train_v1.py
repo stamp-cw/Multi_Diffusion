8f52d2520057353bb527c1a0827a63b388d9a9aa
AAA <AAA@AAA.COM>
2025-05-06 11:47:26 +0800

    "multi process add"

--- src/train/mp_train_v1.py
+++ src/train/mp_train_v1.py
@@ -18,16 +18,6 @@ from tensorboardX import SummaryWriter
 import matplotlib.pyplot as plt
 import logging
 
-
-import os
-import torch
-import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.utils.data import Dataset, DataLoader
-from torch.utils.data.distributed import DistributedSampler
-
-
 from src.diffusions.gamma_diffusion import GammaDiffusion
 from src.diffusions.gaussian_diffusion import GaussianDiffusion
 from src.diffusions.binomial_diffusion import BinomialDiffusion
@@ -35,14 +25,49 @@ from src.diffusions.negative_binomial_diffusion import NBinomialDiffusion
 from src.diffusions.possion_diffusion import PossionDiffusion
 from src.diffusions.optimize_gamma_diffusion import OGammaDiffusion
 
+
 logging.basicConfig(level=logging.INFO)
 
 ####################################################################################################
 # 训练
 ####################################################################################################
 
+def train(config):
+    """
+    Train
+    :param config:
+    :return:
+    """
+    logging.info("Start Train...")
+    root_dir = config['root_dir']
+    batch_size = config['model_config']['batch_size']
+    timesteps = config['model_config']['timesteps']
+    beta_schedule = config['model_config']['beta_schedule']
+    datasets_type = config['datasets_type']
+    epochs = config['epochs']
+    group_epoch = config['group_epoch']  # 多少个epoch 为一组 ，用于记录
+    RESUME = True if config['resume']=="True" else False
+    diffusion_type=config['diffusion_type']
+    exper_type = config['exper_type']
+    exper_name = f"{config['exper_type']}_{config['diffusion_type']}_{config['datasets_type']}_{config['epochs']}_{config['resume']}_{config['exper_num']}"
+    eval_subprocess = config['eval_subprocess']
 
-def load_dataset(root_dir,datasets_type):
+
+    diffusion_dict = {
+        "Binomial":BinomialDiffusion,
+        "NBinomial":NBinomialDiffusion,
+        "Gaussian":GaussianDiffusion,
+        "Gamma": GammaDiffusion,
+        "Possion":PossionDiffusion,
+        "OGamma":OGammaDiffusion,
+    }
+    if diffusion_type in diffusion_dict.keys() :
+        diffusion = diffusion_dict[diffusion_type](timesteps=timesteps, beta_schedule=beta_schedule)
+
+    # 设置训练设备
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    logging.debug(f"Already used <{device}>")
+
     # 加载数据集
     if datasets_type == "cifar10":
         transform = transforms.Compose([
@@ -86,60 +111,7 @@ def load_dataset(root_dir,datasets_type):
         dataset_channel = 3
         dataset_image_size = 64
 
-    return dataset,dataset_channel,dataset_image_size
-
-
-def train(rank,world_size,config):
-    """
-    Train
-    :param config:
-    :return:
-    """
-
-    # 环境变量
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-    # os.environ["GLOO_USE_LIBUV"] = "0"
-    dist.init_process_group("nccl",init_method="env://", rank=rank, world_size=world_size)
-
-
-    logging.info("Start Train...")
-    root_dir = config['root_dir']
-    batch_size = config['model_config']['batch_size']
-    timesteps = config['model_config']['timesteps']
-    beta_schedule = config['model_config']['beta_schedule']
-    datasets_type = config['datasets_type']
-    epochs = config['epochs']
-    group_epoch = config['group_epoch']  # 多少个epoch 为一组 ，用于记录
-    RESUME = True if config['resume']=="True" else False
-    diffusion_type=config['diffusion_type']
-    exper_type = config['exper_type']
-    exper_name = f"{config['exper_type']}_{config['diffusion_type']}_{config['datasets_type']}_{config['epochs']}_{config['resume']}_{config['exper_num']}"
-    eval_subprocess = config['eval_subprocess']
-
-
-    diffusion_dict = {
-        "Binomial":BinomialDiffusion,
-        "NBinomial":NBinomialDiffusion,
-        "Gaussian":GaussianDiffusion,
-        "Gamma": GammaDiffusion,
-        "Possion":PossionDiffusion,
-        "OGamma":OGammaDiffusion,
-    }
-    if diffusion_type in diffusion_dict.keys() :
-        diffusion = diffusion_dict[diffusion_type](timesteps=timesteps, beta_schedule=beta_schedule)
-
-    # 设置训练设备
-    # device = "cuda" if torch.cuda.is_available() else "cpu"
-    torch.cuda.set_device(rank)
-    logging.debug(f"Already used <{rank}>")
-
-    dataset, dataset_channel, dataset_image_size = load_dataset(root_dir,datasets_type)
-
-    sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=True)
-
-
-    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True,sampler=sampler)
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     # 创建unet_model
     unet_model = UNetModel(
@@ -150,8 +122,6 @@ def train(rank,world_size,config):
         attention_resolutions=(2,),
         dropout=0.1
     ).float()
-
-    unet_model = DDP(unet_model, device_ids=[rank])
 
     # 设置优化器
     optimizer = torch.optim.Adam(unet_model.parameters(), lr=2e-4)
@@ -166,18 +136,14 @@ def train(rank,world_size,config):
         optimizer.load_state_dict(check_point["optimizer_state_dict"])
         start_epoch = check_point["epoch"]
 
-
-    device = rank
-
     # 统一张量的device
     unet_model = unet_model.to(device)
 
     # Unet 训练 噪声
     for epoch in range(start_epoch + 1, epochs + 1):
-        print(f"即将训练 第{epoch}轮,Rank {rank}")
+        print(f"即将训练 第{epoch}轮")
 
         for step, (images, labels) in enumerate(train_loader):
-            # images = images.to(rank)
             optimizer.zero_grad()
             batch_size = images.shape[0]
             images = images.to(device)
@@ -197,7 +163,7 @@ def train(rank,world_size,config):
             config['checkpoint_path'] = rf"{root_dir}/checkpoints/{exper_name}_{epoch}.pth"
             config['ok_epoch'] = epoch
             q.put(config)
-    dist.destroy_process_group()
+
 
 if __name__ == '__main__':
     # 获取命令行参数
@@ -226,12 +192,8 @@ if __name__ == '__main__':
 
     # tensorboar 记录
     writer = SummaryWriter(rf'{logs_dir}/{experiment_name}',flush_secs=120)
-
-    world_size = torch.cuda.device_count()
-    print(f"Found {world_size} GPUs.")
-    mp.spawn(train, args=(world_size,train_config), nprocs=world_size, join=True)
     # 开始训练
-    # train(train_config)
+    train(train_config)
     # 结尾工作
     writer.close()
 
